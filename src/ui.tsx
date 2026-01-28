57f96f1dc553823f95b22254aa3a440aec52bf7f
nearnshaw <earnshaw.nico@gmail.com>
2026-01-28 14:13:10 -0300

    screen scale

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,1 +1,12 @@
-import ReactEcs, { UiEntity } from "@dcl/sdk/react-ecs"
+import ReactEcs, { ReactEcsRenderer, UiEntity } from "@dcl/sdk/react-ecs"
+
+
+export function setupUi() {
+    ReactEcsRenderer.setUiRenderer(uiMenu, { virtualWidth: 1920, virtualHeight: 1080 })
+}
+
+// draw your UI here
+export const uiMenu = () => (
+    <UiEntity>  
+    </UiEntity>
+)
